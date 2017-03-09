ea72e940c302d1c7fb3da133321668c76a10b56c
Thomas Uebel <t.uebel@gmail.com>
2017-03-09 20:42:37 +0100

    Create runnable image and executable provisioning

--- imgrun.sh
+++ imgrun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name mysql-docker -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mysql:5.7
+docker run --name mysql-docker -e MYSQL_ROOT_PASSWORD=developer -p 3306:3306 -d mysql-docker
---
+++ provision.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec mysql-docker ./var/scripts/create-baseschema.sh
