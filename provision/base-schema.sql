4071e8c01cef4d6e3e0c56af1d4a02a66b27b7a9
Thomas Uebel <thomas.uebel@misterspex.de>
2018-05-15 10:53:45 +0200

    Change the way scripts are called. Basescheme creation is it's own file.

---
+++ provision/base-schema.sql
@@ -0,0 +1,4 @@
+CREATE SCHEMA IF NOT EXISTS baseschema CHARACTER SET utf8;
+GRANT ALL PRIVILEGES ON baseschema.* TO 'baseschema'@'localhost' IDENTIFIED BY 'baseschema';
+GRANT ALL PRIVILEGES ON baseschema.* TO 'baseschema'@'%' IDENTIFIED BY 'baseschema';
+
--- provision/run-scripts.sh
+++ provision/run-scripts.sh
@@ -7,12 +7,6 @@ done
 # give it another 5s to accept user connections
 sleep 5s
 
-# create an empty baseschema
-mysql --user=root -pdeveloper --execute \
- "CREATE SCHEMA IF NOT EXISTS baseschema CHARACTER SET utf8; \
-  GRANT ALL PRIVILEGES ON baseschema.* TO 'baseschema'@'localhost' IDENTIFIED BY 'baseschema'; \
-  GRANT ALL PRIVILEGES ON baseschema.* TO 'baseschema'@'%' IDENTIFIED BY 'baseschema';"
-
 # run all provided sql snippets
 for filename in /var/scripts/*.sql; do
 	mysql --user=root -pdeveloper < $filename
